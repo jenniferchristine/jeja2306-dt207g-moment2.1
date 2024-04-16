b700272034f05c0fa7ac01688d2560e55ec5df1f
jenniferchristine <jeja2306@student.miun.se>
2024-04-16 18:03:00 +0200

    added logs for errors

--- install.js
+++ install.js
@@ -1,8 +1,7 @@
 const { Client } = require("pg");
 require("dotenv").config();
 
-// anslut till databas
-const client = new Client({
+const client = new Client({ // åtkomst för anslut
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
@@ -13,10 +12,26 @@ const client = new Client({
     },
 });
 
-client.connect((err) => {
+client.connect((err) => { // anslutning
     if (err) {
         console.log("Could not connect to database " + err);
     } else {
         console.log("Connected to database");
     }
 });
+
+client.query(`
+    CREATE TABLE cv(
+    id SERIAL PRIMARY KEY,
+    companyname VARCHAR(255) NOT NULL,
+    jobtitle VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    decription TEXT,
+    startdate DATE,
+    enddate DATE)`, (err, results) => {
+        if (err) {
+            console.log("Error: Could not create table " + err);
+            return;
+        }
+        console.log("Table created " + results);
+    });
